869f818e598c76a528ca0a95b4723f4dd89e43cb
Awesomeplayer165 <dev@awesomeplayer.tech>
2022-12-27 21:26:14 -0800

    initial commit

---
+++ example/frc_8033_scouting_shared_example.dart
@@ -0,0 +1,6 @@
+import 'package:frc_8033_scouting_shared/frc_8033_scouting_shared.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/frc_8033_scouting_shared.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library frc_8033_scouting_shared;
+
+export 'src/frc_8033_scouting_shared_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
