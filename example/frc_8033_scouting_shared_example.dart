6bfc5d6f9d26c92033d9cafee0ec3ca0c7aca4ff
Awesomeplayer165 <dev@awesomeplayer.tech>
2022-12-30 21:50:34 -0800

    Added files hopefully

--- example/frc_8033_scouting_shared_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:frc_8033_scouting_shared/frc_8033_scouting_shared.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/frc_8033_scouting_shared.dart
+++ lib/frc_8033_scouting_shared.dart
@@ -3,6 +3,7 @@
 /// More dartdocs go here.
 library frc_8033_scouting_shared;
 
-export 'src/frc_8033_scouting_shared_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/climbing_challenge.dart';
+export 'src/event_types.dart';
+export 'src/game_screen_positions.dart';
+export 'src/robot_roles.dart';
