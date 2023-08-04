85c6fe98860b5bda05ffb9aba32bf32100645e1c
MangoSwirl <collin.camerono@gmail.com>
2023-08-04 14:21:59 -0700

    Remove unused imports

--- lib/src/match.dart
+++ lib/src/match.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class GameMatchIdentity {
   GameMatchIdentity(
     this.type,
--- lib/src/scout_schedule.dart
+++ lib/src/scout_schedule.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:lzstring/lzstring.dart';
